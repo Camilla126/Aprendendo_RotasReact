34ea08532b08f9e73101fceafbe29615ba8b4fe9
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-10-24 11:10:32 -0300

    Iniciando projeto de aprendizado

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function App() {
+  const [tarefas, setTarefas] = useState(['pagar a conta'])
+  return (
+<div>
+<h1>
+  hook
+</h1>
+  </div>
+
+
+  ) ;
+}
+export default App;
