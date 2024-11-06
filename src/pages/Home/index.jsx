341dfa99155946e619e3a68aa5df2bfce3e359f6
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-11-06 11:23:54 -0300

    Aula de rotas concluída

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,6 +9,10 @@ function Home() {
 
 <Link to='/sobre'>Sobre</Link> <br />
 <Link to='/contato'>Contato</Link>
+
+<hr />
+
+<Link to="/produto/123">Acessar Produto 123</Link>
  </div>
  );}
 export default Home
